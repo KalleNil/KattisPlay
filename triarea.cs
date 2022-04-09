8234b4106f1141f6e075ab98e005756dc8500a0c
Kalle <domare2@hotmail.com>
2022-04-09 23:05:29 +0200

    triarea.cs

---
+++ triarea.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace SammaSolutionNyaKattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            Console.WriteLine((double)Convert.ToInt32(input[0]) * Convert.ToInt32(input[1])/2);
+        }
+    }
+}
